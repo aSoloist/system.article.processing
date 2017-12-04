06454af11a1400a5b1bfa582667fe25d25547fca
Soloist <947721002@qq.com>
2017-12-04 21:39:08 +0800

    添加用户类

--- src/com/article/processing/model/User.java
+++ src/com/article/processing/model/User.java
@@ -1,7 +1,100 @@
 package com.article.processing.model;
 
+import java.util.UUID;
+
 /**
+ * 用户类
  * Created by Soloist on 2017/12/4 19:55
  */
 public class User {
+    //id
+    String id = UUID.randomUUID().toString();
+    //昵称
+    String nickname;
+    //密码
+    String password;
+    //姓名
+    String username;
+    //单位
+    String unit;
+    //地址
+    String address;
+    //手机号
+    String phone;
+    //邮箱
+    String email;
+
+    int status = 0;
+    
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
 }

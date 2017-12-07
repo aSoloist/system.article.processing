227b5c187b4851eb19895641ba8ce68db57f88a3
Soloist <947721002@qq.com>
2017-12-07 21:05:43 +0800

    添加web.xml

--- src/com/article/processing/servlet/BaseServlet.java
+++ src/com/article/processing/servlet/BaseServlet.java
@@ -1,5 +1,8 @@
 package com.article.processing.servlet;
 
+import com.article.processing.dao.UserDao;
+import com.article.processing.dao.impl.UserDaoImpl;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -10,9 +13,10 @@ import java.io.IOException;
  * Created by Soloist on 2017/12/4 20:13
  */
 public class BaseServlet extends HttpServlet {
-
+    UserDao userDao = new UserDaoImpl();
+    
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        
+        userDao.getAll();
     }
 }

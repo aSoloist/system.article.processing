fda43d7aa97b21a8c21843ea443c55b2a799d828
Soloist <947721002@qq.com>
2017-12-14 00:02:45 +0800

    MD5加密工具类

---
+++ src/com/article/processing/utils/MD5Util.java
@@ -0,0 +1,42 @@
+package com.article.processing.utils;
+
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * Created by Soloist on 2017/12/13 20:56
+ */
+public class MD5Util {
+    private static MessageDigest md5;
+    private final static String KEY = "auth-token";
+    
+    static {
+        try {
+            md5 = MessageDigest.getInstance("MD5");
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public static String eccrypt(String message) {
+        byte[] bytes = new byte[0];
+        try {
+            bytes = (message + KEY).getBytes("UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        byte[] result = md5.digest(bytes);
+        StringBuilder ret = new StringBuilder(bytes.length<<1);
+        for (byte aByte : result) {
+            ret.append(Character.forDigit((aByte >> 4) & 0xf, 16));
+            ret.append(Character.forDigit(aByte & 0xf, 16));
+        }
+        return ret.toString();
+    }
+
+    public static void main(String[] args) {
+        String m = "947721002@qq.com";
+        System.out.println(eccrypt(m));
+    }
+}

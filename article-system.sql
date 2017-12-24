0c9a108095bd750cc9e21a75fab2caae12e9ca53
Soloist <947721002@qq.com>
2017-12-24 14:59:59 +0800

    数据库

--- article-system.sql
+++ article-system.sql
@@ -30,6 +30,7 @@ CREATE TABLE `article` (
   `create_time` datetime NOT NULL,
   `article_group` varchar(36) NOT NULL,
   `status` int(11) NOT NULL,
+  `ver` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `Article_user_id_fk` (`user_id`),
   CONSTRAINT `Article_user_id_fk` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
@@ -42,7 +43,6 @@ CREATE TABLE `article` (
 
 LOCK TABLES `article` WRITE;
 /*!40000 ALTER TABLE `article` DISABLE KEYS */;
-INSERT INTO `article` VALUES ('77f8785a-6fdc-4b18-ae4b-3186b2041f38','测试','测试','deb6b563-02f4-49a2-b165-aafd1661e008','2017-12-14 21:20:56','5735c452-7876-445a-9f48-72ef91f607ea',0),('77f8785a-6fdc-4b18-ae4b-3186b2041f45','测试','更新测试','deb6b563-02f4-49a2-b165-aafd1661e008','2017-12-14 21:21:56','5735c452-7876-445a-9f48-72ef91f607ea',0),('c0621067-18be-43ae-a7df-4a62169ff9ac','测试','又一个测试','deb6b563-02f4-49a2-b165-aafd1661e008','2017-12-14 21:36:44','e7d5b7d6-2798-454a-abef-d4711258aa5a',0);
 /*!40000 ALTER TABLE `article` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -59,6 +59,7 @@ CREATE TABLE `message` (
   `message_content` text NOT NULL COMMENT '消息内容',
   `create_time` datetime NOT NULL COMMENT '消息创建时间',
   `status` int(11) NOT NULL COMMENT '消息状态 0：未读 -1：删除',
+  `title` varchar(30) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `message_user_id_fk` (`send_id`),
   CONSTRAINT `message_user_id_fk` FOREIGN KEY (`send_id`) REFERENCES `user` (`id`)
@@ -92,9 +93,7 @@ CREATE TABLE `user` (
   `email` varchar(36) NOT NULL,
   `status` int(11) NOT NULL,
   `create_time` datetime NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `user_phone_uindex` (`phone`),
-  UNIQUE KEY `user_email_uindex` (`email`)
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -104,7 +103,6 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES ('4e414e69-a6d1-4e75-9db8-a46e9d353f1d','测试','10cfc060bd294106f06211685c05ca58','姓名','单位','地址','11111111111','947721002@qq.com',10,'2017-12-18 22:16:06'),('deb6b563-02f4-49a2-b165-aafd1661e008','Soloist','f9ba019742a5d237d9895765740c4525','梁焱','测试','测试','13137020207','soloist1511@gmail.com',101,'2017-12-13 23:48:17');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -117,4 +115,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-12-20 16:11:15
+-- Dump completed on 2017-12-24 14:58:43
